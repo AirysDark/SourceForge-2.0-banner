e1610319a804a1f5bd0ada3984807796f89d017d
AirysDark <megamanx2sonic@gmail.com>
2025-10-01 09:59:04 -0700

    u

--- banner.d/50-ram.sh
+++ banner.d/50-ram.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-free -h | awk 'NR==2{print "[SF2] RAM: "$3" / "$2"}'
+# Try MiB with percentage; fallback human readable
+if free -b >/dev/null 2>&1; then
+  free -b | awk 'NR==2{printf "[SF2] RAM: %.1fGiB / %.1fGiB (%.0f%%)\n", $3/1024/1024/1024, $2/1024/1024/1024, ($3/$2)*100}'
+else
+  free -h | awk 'NR==2{print "[SF2] RAM: "$3" / "$2}'
+fi
