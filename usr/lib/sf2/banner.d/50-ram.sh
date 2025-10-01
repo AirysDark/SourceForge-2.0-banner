c39e00e32c6f3343c2591871605c204a9cca8115
AirysDark <megamanx2sonic@gmail.com>
2025-10-01 14:34:13 -0700

    h

--- usr/lib/sf2/banner.d/50-ram.sh
+++ usr/lib/sf2/banner.d/50-ram.sh
@@ -1,4 +1,2 @@
-#!/usr/bin/env bash
-set -euo pipefail
-read -r total used perc <<<"$(free -m | awk '/Mem:/ {printf "%dMi %dMi %.0f%%", $2, $3, ($3/$2)*100}')"
-echo "RAM: ${used} / ${total} (${perc})"
+#!/bin/sh
+free -h | awk 'NR==2{print "[SF2] RAM: "$3" / "$2}'"
