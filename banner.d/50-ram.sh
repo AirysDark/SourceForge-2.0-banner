e1bbc7099c62fea328e76c15d7160264707d3aa1
AirysDark <megamanx2sonic@gmail.com>
2025-10-01 10:33:31 -0700

    Update 50-ram.sh

--- banner.d/50-ram.sh
+++ banner.d/50-ram.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# 50-ram.sh — show RAM usage
+
 if free -b >/dev/null 2>&1; then
-  free -b | awk 'NR==2{printf "[SF2] RAM: %.1fGiB / %.1fGiB (%.0f%%)\n", $3/1024/1024/1024, $2/1024/1024/1024, ($3/$2)*100}'
+  free -b | awk 'NR==2 {printf "[SF2] RAM: %.1fGiB / %.1fGiB (%.0f%%)\n", $3/1024/1024/1024, $2/1024/1024/1024, ($3/$2)*100}'
 else
-  free -h | awk 'NR==2{print "[SF2] RAM: "$3" / "$2}''
+  free -h | awk 'NR==2 {print "[SF2] RAM: "$3" / "$2}'
 fi
