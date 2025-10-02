7814ee7f13652aad806bd54a5f9fadc753e4a309
AirysDark <megamanx2sonic@gmail.com>
2025-10-01 18:03:43 -0700

    v

--- usr/lib/sf2/banner.d/10-hostname.sh
+++ usr/lib/sf2/banner.d/10-hostname.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . /usr/lib/sf2/colors.sh
-echo -e " ${C_WHITE}-${C_RST} ${C_BLUE}Hostname:${C_RST} $(hostname)"
+printf " %s-%s %sHostname:%s %s%s%s\n" "$C_BUL" "$C_RST" "$C_LABEL" "$C_RST" "$C_VAL" "$(hostname)" "$C_RST"
